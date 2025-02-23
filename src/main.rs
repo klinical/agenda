a09764efbe34101081934c642621a6815ef8f1a3
Zach <zachary.nickels@clearfly.net>
2025-02-23 16:36:10 -0600

    remove 'help' info line

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use agenda::command::Command;
 fn main() {
     let mut database = Database::open(constants::F_PATH.as_ref()).expect("Failed to open/create database. Ensure this application is run with all required permissions.");
     println!("** AGENDA - A simple todo app from the 90's!");
-    println!("** You may list the available commands by running 'help'.\n");
     let actions = vec![Command::Add, Command::Mod, Command::Remove, Command::List, Command::Exit];
     loop {
         let selection = Select::with_theme(&constants::select_theme())
